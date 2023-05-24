fcd3b9768ca58be2e96a4b8561e652f356fa948d
jakubmikoxv <jakubmiko54@gmail.com>
2023-05-24 17:45:39 +0200

    yes

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useRef } from 'react';
+import './App.css';
+import Herta from '/Audio/Herta.mp3';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const audioRef = useRef(null);
+
+  const handlePlayAudio = () => {
+    audioRef.current.play();
+  };
+
+  return (
+    <>
+      <div>
+        <img src="/image/kurukuru-kururing.gif" alt="" />
+      </div>
+      <h1>Jakub Mikołajczyk 113769</h1>
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)}>spin counter {count}</button>
+        <p>
+          <button onClick={handlePlayAudio}>Kururin~</button>
+        </p>
+        <audio ref={audioRef}>
+          <source src={Herta} type="audio/mp3" />
+        </audio>
+      </div>
+      <p className="read-the-docs"></p>
+    </>
+  );
+}
+
+export default App;
